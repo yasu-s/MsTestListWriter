19e1fa364add0d13202e029f39ef74f2e12f13dd
yasu-s <yasuyuki.saito.2012@gmail.com>
2013-07-04 20:27:02 +0900

    プロジェクトファイル等を追加

---
+++ Sources/MsTestListWriter/MsTestData.cs
@@ -0,0 +1,34 @@
+﻿namespace MsTestListWriter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public sealed class MsTestData
+    {
+        public string TypeName
+        {
+            get;
+            set;
+        }
+
+        public string MethodName
+        {
+            get;
+            set;
+        }
+
+        public string[] Categories
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
